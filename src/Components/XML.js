504e50674b59fb0d44baaa348e5e3ca42c75f644
Amir <kechillosh@gmail.com>
2023-06-06 03:50:55 +0300

    oo

--- src/Components/XML.js
+++ src/Components/XML.js
@@ -9,39 +9,39 @@
         <button className='button_xml'>masini sub 500 lei</button>
         </Link>
         
-        <Link to=''>
+        <Link to='/peste500'>
         <button className='button_xml'>masini peste 500 lei</button>
         </Link>
 
-        <Link to=''>
+        <Link to='/peste30'>
         <button className='button_xml'>masini peste 30000 km</button>
         </Link>
 
-        <Link to=''>
+        <Link to='/sub30'>
         <button className='button_xml'>masini sub 30000 km</button>
         </Link>
 
-        <Link to=''>
+        <Link to='/SortAlb'>
         <button className='button_xml'>masini albe</button>
         </Link>
 
-        <Link to=''>
+        <Link to='/SortNegru'>
         <button className='button_xml'>masini negre</button>
         </Link>
 
-        <Link to=''>
+        <Link to='TipC'>
         <button className='button_xml'>Tip Carburant Benzina</button>
         </Link>
 
-        <Link to=''>
+        <Link to='/Media'>
         <button className='button_xml'>Media de km a masinilor</button>
         </Link>
 
         <Link to='/Lux'>
         <button className='button_xml'>masini lux</button>
         </Link>
-        
-        <Link to=''>
+
+        <Link to='/sport'>
         <button className='button_xml'>masini sport</button>
         </Link>
 
