338906a68dbe094c7a290f69aebab8d5f1a2c124
Amir <kechillosh@gmail.com>
2023-06-06 03:48:20 +0300

    DA

--- src/Components/XML.js
+++ src/Components/XML.js
@@ -5,33 +5,42 @@
  function XML() {
    return (
      <div> 
-        <Link to=''>
+        <Link to='/sub500'>
         <button className='button_xml'>masini sub 500 lei</button>
         </Link>
-        <Link to=''>
+        
         <Link to=''>
         <button className='button_xml'>masini peste 500 lei</button>
         </Link>
+
         <Link to=''>
         <button className='button_xml'>masini peste 30000 km</button>
         </Link>
+
         <Link to=''>
         <button className='button_xml'>masini sub 30000 km</button>
         </Link>
+
         <Link to=''>
         <button className='button_xml'>masini albe</button>
         </Link>
+
         <Link to=''>
         <button className='button_xml'>masini negre</button>
         </Link>
+
         <Link to=''>
         <button className='button_xml'>Tip Carburant Benzina</button>
         </Link>
+
+        <Link to=''>
         <button className='button_xml'>Media de km a masinilor</button>
         </Link>
+
         <Link to='/Lux'>
         <button className='button_xml'>masini lux</button>
         </Link>
+        
         <Link to=''>
         <button className='button_xml'>masini sport</button>
         </Link>
